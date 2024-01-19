4f93f132cd69b29afb7eccb06dfe550d39672ade
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-01-18 21:10:14 -0500

    yes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,44 +6,31 @@ import company from '../assets/icon-company.svg'
 import twitter from '../assets/icon-twitter.svg'
 import website from '../assets/icon-website.svg'
 
-export default function Body() {
- 
-const [user, setUser] = useState('')
+export default function Body({data}) {
 
-// gets users date of account creation
- const eta = user.created_at
+
+//gets users date of account creation
+ const eta = data.created_at
  const date = new Date(eta);
  const newdate = date.toDateString()
  
  
 
 
- const username= 'octacat' 
+ 
 
-useEffect(() => {
-  async function fetchData () {
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`)
-      const result = await response.json();
-      setUser(result)
-      console.log(user)
-    }  catch (error) {
-      console.log(error)
-    }
-  }
-  fetchData()
-},[])
+
 
 
   return (
     <div className={classes.container}>
       <div className={classes.user}>
-        <img src={user.avatar_url} />
+        <img src={data.avatar_url} />
         <div className={classes.userinfo}>
-          <h1>{user.login}</h1>
-          <a><h3>@{user.login}</h3></a>
+          <h1>{data.login}</h1>
+          <a><h3>@{data.login}</h3></a>
           <p>
-            joined {newdate}
+            {newdate}
           </p>
         </div>
       </div>
@@ -54,33 +41,33 @@ useEffect(() => {
       <div className={classes.stats}>
         <div className={classes.sta}>
           <h5>Repos</h5>
-          <span>{user.public_repos }</span>
+          <span>{data.public_repos }</span>
         </div>
         <div className={classes.sta}>
           <h5>Followers</h5>
-          <span>{user.followers}</span>
+          <span>{data.followers}</span>
         </div>
         <div className={classes.sta}>
           <h5>Following</h5>
-          <span>{user.following}</span>
+          <span>{data.following}</span>
         </div>
       </div>
       <div className={classes.sociallinks}>
         <div className={classes.sl}>
           <img src={location}/>
-          <span> {user.location? user.location : "not available"}</span>
+          <span> {data.location? data.location : "not available"}</span>
         </div>
         <div className={classes.sl}>
           <img src={website}/>
-          <span> {user.html_url ? user.html_url : 'not available'}</span>
+          <span> {data.html_url ? data.html_url : 'not available'}</span>
         </div>
         <div className={classes.sl}>
           <img src={twitter}/>
-          <span>{user.twitter_username ? user.twiitter_username : "not available"}</span>
+          <span>{data.twitter_username ? data.twiitter_username : "not available"}</span>
         </div>
         <div className={classes.sl}>
           <img src={company}/>
-          <span className={classes.twit}>@{user.company ? user.company :'not available'}</span>
+          <span className={classes.twit}>@{data.company ? data.company :'not available'}</span>
         </div>
       </div>
     </div>
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -4,14 +4,30 @@ import Searchbar from '../components/searchbar'
 import { useState, useEffect } from 'react'
 
 export default function Mainpage() {
-  
 
+  const [user, setUser] = useState([])
+  const [username, setUsername] =useState('octacat')
+  
+  useEffect(() => {
+    async function fetchData () {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const result = await response.json();
+        setUser(result)
+        console.log(result)
+        console.log(user)
+      }  catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[])
   
   return (
     <div>
       <Header />
-      <Searchbar />
-      <Body   />
+      <Searchbar setUsername={setUsername} username={username}/>
+      <Body  data={user}/>
     </div>
   )
 }
