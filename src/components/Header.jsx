ebe45f3d078a450f80acdabf4657a365f3f7c5b7
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-01-28 19:12:42 -0500

    ues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,16 @@ import { useContext } from 'react'
 import sun from '../assets/icon-sun.svg'
 export default function Header() {
   const {theme} = useContext(ThemeContext)
+  const {toggleTheme} = useContext(ThemeContext)
 
-function Click () {
-console.log(theme)
-}
+
+
 
   return (
     <div className={theme + 1} >
         <h3>devfinder</h3>
         <div className='kidtainer'>
-            <button onClick={Click} >{theme === "dark" ? 'Light' : "Dark" }</button>
+            <button onClick={toggleTheme} >{theme === "dark" ? 'Light' : "Dark" }</button>
             {theme === 'light' ? <img src={moon}/> : <img src={sun}/>}   
         </div>
     </div>
